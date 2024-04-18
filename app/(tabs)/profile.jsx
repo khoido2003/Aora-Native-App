16e4c5fb6350c2b85296e5a78da0ff0c78adb8b3
Khoi Do <khoizpro31@gmail.com>
2024-04-18 14:47:33 +0700

    Create onBoarding screen

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: "#000",
+      }}
+    >
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
